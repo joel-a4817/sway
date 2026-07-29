ce259d222a427bd1a226ea8b7602dedcbe385137
rt4817 <joel.ag789@gmail.com>
2026-07-29 21:19:57 +1000

    same active window for swaynag

--- scripts/start-swaynag.sh
+++ scripts/start-swaynag.sh
@@ -4,6 +4,8 @@
 pkill swaynag 2>/dev/null || true
 pkill wvkbd-deskintl 2>/dev/null ||true
 
+OUT="$(swaymsg -t get_outputs -r | jq -r '.[] | select(.focused) | .name')"
+
 # Small delay to ensure the old layer surface is gone
 sleep 0.05
 
@@ -12,7 +14,7 @@ wvkbd-deskintl --hidden --alpha 216 -L 500 -H 920 --fn "JetBrainsMono Nerd Font 
 exec swaynag \
   --edge bottom \
   --layer overlay \
-  --output eDP-1 \
+  --output "$OUT" \
   --message "" \
   --button-no-terminal " ⌨ " "pkill -RTMIN wvkbd" \
   --background 000000AA \
