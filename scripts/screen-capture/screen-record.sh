fe4fb6f1bad3aeaddcec73ad61aa7a5a2e497e2a
rt4817 <joel.ag789@gmail.com>
2026-08-01 11:25:53 +1000

    screen-rec works, screenshot will

--- scripts/screen-capture/screen-record.sh
+++ scripts/screen-capture/screen-record.sh
@@ -4,17 +4,15 @@ set -euo pipefail
 
 OUT="$(swaymsg -t get_outputs -r | jq -r '.[] | select(.focused) | .name')"
 
-mkdir -p ~/Media/Videos/Screen-recordings
+mkdir -p "~/Media/Videos/Screenrecordings"
 
-FILE="~/Media/Videos/Screen-recordings/recording-$(date +%Y%m%d-%H%M%S).mkv"
-
-if pgrep -x wf-recorder >/dev/null; then
+if pgrep -f wf-recorder >/dev/null; then
     swaynag \
         -t warning \
         -o "$OUT" \
         -y overlay \
         -m "Recording in progress on $OUT" \
-        -z "Stop Recording" "pkill -x wf-recorder"
+        -z "Stop Recording" "pkill -f wf-recorder"
 else
     swaynag \
         -t warning \
@@ -22,8 +20,7 @@ else
         -y overlay \
         -m "Start recording on $OUT" \
         -z "Without Audio" \
-            "wf-recorder -a --audio-backend=pipewire -o \"$OUT\" -f \"$FILE\"" \
+        "wf-recorder -a --audio-backend=pipewire -o "$OUT" -f ~/Media/Videos/Screen-recordings/recording-\$(date +%Y%m%d-%H%M%S).mkv" \
         -z "With Audio" \
-            "wf-recorder -a -o \"$OUT\" -f \"$FILE\""
+        "wf-recorder -a -o "$OUT" -f ~/Media/Videos/Screen-recordings/recording-\$(date +%Y%m%d-%H%M%S).mkv"
 fi
-
--- scripts/screen-capture/screenshot.sh
+++ scripts/screen-capture/screenshot.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 OUT="$(swaymsg -t get_outputs -r | jq -r '.[] | select(.focused) | .name')"
 
+mkdir -p "~/Media/Pictures/Screenshots"
+
 swaynag \
     -t warning \
     -y overlay \
@@ -12,8 +14,8 @@ swaynag \
     -z "Copy Fullscreen" \
         "grim -o "$OUT" -t png - | wl-copy" \
     -z "Save & Copy Fullscreen" \
-        "mkdir -p ~/Media/Pictures/Screenshots && grim -o "$OUT" -t png - | tee ~/Media/Pictures/Screenshots/screenshot-\$(date +%Y%m%d-%H%M%S).png | wl-copy" \
+        "grim -o "$OUT" -t png - | tee ~/Media/Pictures/Screenshots/screenshot-\$(date +%Y%m%d-%H%M%S).png | wl-copy" \
     -z "Copy Selection" \
-        "grim -o "$OUT" -t png -g \"\$(slurp)\" - | wl-copy" \
+        "grim -o "$OUT" -t png -g "\$(slurp)" - | wl-copy" \
     -z "Save & Copy Selection" \
-        "mkdir -p ~/Media/Pictures/Screenshots && grim -o "$OUT" -t png -g \"\$(slurp)\" - | tee ~/Media/Pictures/Screenshots/screenshot-\$(date +%Y%m%d-%H%M%S).png | wl-copy"
+        "grim -o "$OUT" -t png -g "\$(slurp)" - | tee ~/Media/Pictures/Screenshots/screenshot-\$(date +%Y%m%d-%H%M%S).png | wl-copy"
