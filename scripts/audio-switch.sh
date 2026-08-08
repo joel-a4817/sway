8ca01f7375d83d0ee8f76241720017997c5ba3d1
rt4817 <joel.ag789@gmail.com>
2026-08-08 19:49:16 +1000

    this isn't final, but audio should work for both

--- scripts/audio-switch.sh
+++ scripts/audio-switch.sh
@@ -10,38 +10,7 @@ touch "$ACTION_LOG"
 
 export RESULT_FILE ACTION_LOG
 
-# Build a list of sinks:
-# sink_name|description
-mapfile -t SINKS < <(
-    pactl list sinks |
-    awk '
-        /^Sink #/ {
-            if (name != "" && desc != "")
-                print name "|" desc
-            name=""
-            desc=""
-        }
-
-        /^[[:space:]]*Name:/ {
-            name=$2
-        }
-
-        /^[[:space:]]*Description:/ {
-            sub(/^[[:space:]]*Description:[[:space:]]*/, "")
-            desc=$0
-        }
-
-        END {
-            if (name != "" && desc != "")
-                print name "|" desc
-        }
-    '
-)
-
-if ((${#SINKS[@]} == 0)); then
-    echo "No audio sinks found."
-    exit 1
-fi
+CARD="$(pactl list cards short | awk 'NR==1 {print $2}')"
 
 ARGS=(
     -t warning
@@ -49,38 +18,26 @@ ARGS=(
     -m "Audio Output Selector"
 )
 
-for entry in "${SINKS[@]}"; do
-    sink="${entry%%|*}"
-    desc="${entry#*|}"
+###############################################################################
+# DETECT MACHINE TYPE
+###############################################################################
 
-    case "$desc" in
-        *HDMI*)
-            label="HDMI"
-            ;;
-        *Speaker*)
-            label="Speaker"
-            ;;
-        *Headphone*)
-            label="Headphones"
-            ;;
-        *Bluetooth*)
-            label="Bluetooth"
-            ;;
-        *USB*)
-            label="USB Audio"
-            ;;
-        *)
-            label="$desc"
-            [[ ${#label} -gt 12 ]] && label="${label:0:12}..."
-            ;;
-    esac
+if pactl list cards | grep -q 'output:hdmi-stereo:'; then
+
+    ###########################################################################
+    # OLD MACHINE (PROFILE BASED)
+    ###########################################################################
 
     ARGS+=(
-        -z "$label"
+        -z "Speaker"
         "
-move_streams() {
-    local sink=\"\$1\"
+pactl set-card-profile \"$CARD\" output:analog-stereo >>\"\$ACTION_LOG\" 2>&1
 
+sleep 1
+
+sink=\$(pactl list short sinks | awk 'NR==1 {print \$2}')
+
+if [[ -n \$sink ]]; then
     pactl set-default-sink \"\$sink\" >>\"\$ACTION_LOG\" 2>&1
 
     pactl list sink-inputs short |
@@ -88,21 +45,158 @@ move_streams() {
         while read -r id; do
             pactl move-sink-input \"\$id\" \"\$sink\" >>\"\$ACTION_LOG\" 2>&1
         done
-}
-
-SINK=\"$sink\"
-
-echo \"Switching to: \$SINK\" >>\"\$ACTION_LOG\"
-
-move_streams \"\$SINK\"
-
-echo \"SUCCESS\" >>\"\$ACTION_LOG\"
-echo \"Sink: \$SINK\" >>\"\$ACTION_LOG\"
+fi
 
 touch \"\$RESULT_FILE\"
 "
     )
-done
+
+    ARGS+=(
+        -z "HDMI1"
+        "
+pactl set-card-profile \"$CARD\" output:hdmi-stereo >>\"\$ACTION_LOG\" 2>&1
+
+sleep 1
+
+sink=\$(pactl list short sinks | awk 'NR==1 {print \$2}')
+
+if [[ -n \$sink ]]; then
+    pactl set-default-sink \"\$sink\" >>\"\$ACTION_LOG\" 2>&1
+
+    pactl list sink-inputs short |
+        awk '{print \$1}' |
+        while read -r id; do
+            pactl move-sink-input \"\$id\" \"\$sink\" >>\"\$ACTION_LOG\" 2>&1
+        done
+fi
+
+touch \"\$RESULT_FILE\"
+"
+    )
+
+    ARGS+=(
+        -z "HDMI2"
+        "
+pactl set-card-profile \"$CARD\" output:hdmi-stereo-extra1 >>\"\$ACTION_LOG\" 2>&1
+
+sleep 1
+
+sink=\$(pactl list short sinks | awk 'NR==1 {print \$2}')
+
+if [[ -n \$sink ]]; then
+    pactl set-default-sink \"\$sink\" >>\"\$ACTION_LOG\" 2>&1
+
+    pactl list sink-inputs short |
+        awk '{print \$1}' |
+        while read -r id; do
+            pactl move-sink-input \"\$id\" \"\$sink\" >>\"\$ACTION_LOG\" 2>&1
+        done
+fi
+
+touch \"\$RESULT_FILE\"
+"
+    )
+
+    ARGS+=(
+        -z "HDMI3"
+        "
+pactl set-card-profile \"$CARD\" output:hdmi-stereo-extra2 >>\"\$ACTION_LOG\" 2>&1
+
+sleep 1
+
+sink=\$(pactl list short sinks | awk 'NR==1 {print \$2}')
+
+if [[ -n \$sink ]]; then
+    pactl set-default-sink \"\$sink\" >>\"\$ACTION_LOG\" 2>&1
+
+    pactl list sink-inputs short |
+        awk '{print \$1}' |
+        while read -r id; do
+            pactl move-sink-input \"\$id\" \"\$sink\" >>\"\$ACTION_LOG\" 2>&1
+        done
+fi
+
+touch \"\$RESULT_FILE\"
+"
+    )
+
+else
+
+    ###########################################################################
+    # HP MACHINE (SINK BASED)
+    ###########################################################################
+
+    mapfile -t SINKS < <(
+        pactl list sinks |
+        awk '
+            /^Sink #/ {
+                if (name != "" && desc != "")
+                    print name "|" desc
+
+                name=""
+                desc=""
+            }
+
+            /^[[:space:]]*Name:/ {
+                name=$2
+            }
+
+            /^[[:space:]]*Description:/ {
+                sub(/^[[:space:]]*Description:[[:space:]]*/, "")
+                desc=$0
+            }
+
+            END {
+                if (name != "" && desc != "")
+                    print name "|" desc
+            }
+        '
+    )
+
+    for entry in "${SINKS[@]}"; do
+
+        sink="${entry%%|*}"
+        desc="${entry#*|}"
+
+        case "$desc" in
+            *HDMI*3*)
+                label="HDMI3"
+                ;;
+            *HDMI*2*)
+                label="HDMI2"
+                ;;
+            *HDMI*1*)
+                label="HDMI1"
+                ;;
+            *Speaker*)
+                label="Speaker"
+                ;;
+            *Headphone*)
+                label="Headphones"
+                ;;
+            *)
+                label="$desc"
+                ;;
+        esac
+
+        ARGS+=(
+            -z "$label"
+            "
+pactl set-default-sink \"$sink\" >>\"\$ACTION_LOG\" 2>&1
+
+pactl list sink-inputs short |
+    awk '{print \$1}' |
+    while read -r id; do
+        pactl move-sink-input \"\$id\" \"$sink\" >>\"\$ACTION_LOG\" 2>&1
+    done
+
+touch \"\$RESULT_FILE\"
+"
+        )
+    done
+fi
+
+###############################################################################
 
 swaynag "${ARGS[@]}" &
 
@@ -119,8 +213,8 @@ fi
 
 rm -f "$RESULT_FILE" "$ACTION_LOG"
 
-echo "Updated default sink:"
-pactl get-default-sink
+echo "Updated sink:"
+pactl get-default-sink 2>/dev/null || true
 
 echo
 read -n 1 -rsp "Press any key to close..."
